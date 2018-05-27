ef86974cfa57d24d776e020034e36cab25bb187f
Michael Sedlak <michi.sedlak@gmail.com>
2018-05-27 02:47:11 +0200

    Setup test environment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as nconf from 'nconf';
 import { DiscordBot } from './discordbot/DiscordBot';
 
-nconf.argv().env().file({file: 'config.json'}).defaults({});
+nconf.argv().env("_").file({file: 'config.json'}).defaults({});
 nconf.required(["discordBot:token", "discordBot:commandPrefix"]);
 
 let discordBot = new DiscordBot(nconf.get("discordBot"));
