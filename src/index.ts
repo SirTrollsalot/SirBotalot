5f5fc02039102be3c355356ad5129feb63632a54
Michael Sedlak <michi.sedlak@gmail.com>
2018-06-07 13:52:02 +0200

    Add config and help text

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,35 @@ import { CommandRouter } from './discordbot/command/CommandRouter';
 import { DiscordAUI } from './discordbot/aui/DiscordAUI';
 import { DiscordPlayer } from './discordbot/player/DiscordPlayer';
 import * as AsciiTable from "ascii-table";
+import { Handler } from './discordbot/command/Handler';
 
 const version = require("../package.json").version;
 
-nconf.argv().env("_").file({file: 'config.json'}).defaults({});
+nconf.argv().env("__").file({file: 'config.json'}).defaults({});
 nconf.required(["discordBot:token", "discordBot:commandPrefix"]);
 
 let logger = getMainLogger();
 let discordBot = new DiscordBot(nconf.get("discordBot"));
 
 let router = new CommandRouter();
-router.use("version", { handle: (cmd, resp) => {
-    resp.reply(`Sir Botalot v${version}`);
-}});
-router.use("help", { handle: (cmd, resp) => {
+router.use("version", (cmd, resp) => resp.reply(`Sir Botalot v${version}`));
+router.use("help", (cmd, resp) => {
     let table = new AsciiTable("Help").setHeading("Command", "Description").removeBorder();
     table.addRow("help", "Print this help text");
-    table.addRow("yt", "Queue a youtube video and start playback");
+    table.addRow("play <song>", "Queue a youtube video and start playback");
+    table.addRow("skip", "Skip current song");
+    table.addRow("stop", "Stop playback");
+    table.addRow("pause", "Pause playback");
+    table.addRow("resume", "Resume playback");
+    table.addRow("pl", "Print current playlist");
     resp.reply("```\n" + table.toString() + "\n```");
-}});
+});
 discordBot.on("command", router.handle.bind(router));
 
 let discordAui = new DiscordAUI({});
 discordBot.on("command", discordAui.handle.bind(discordAui));
 
-let discordPlayer = new DiscordPlayer();
+let discordPlayer = new DiscordPlayer(nconf.get("discordBot:discordPlayer"));
 discordBot.on("command", discordPlayer.handle.bind(discordPlayer));
 
 discordBot.login().then(() => {
