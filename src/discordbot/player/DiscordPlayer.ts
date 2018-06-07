29208db266ffa2cec7df7effcab09ff007c6ec77
Michael Sedlak <michi.sedlak@gmail.com>
2018-06-07 13:54:49 +0200

    Add more player commands

--- src/discordbot/player/DiscordPlayer.ts
+++ src/discordbot/player/DiscordPlayer.ts
@@ -1,114 +1,147 @@
 import { Handler, HandleCallback } from "../command/Handler";
 import { CommandRouter } from "../command/CommandRouter";
 import { GuildCommandContext } from "../command/GuildCommandContext";
-import { EventEmitter } from "events";
-import { Guild, VoiceChannel, VoiceConnection } from "discord.js";
+import { fromS } from "hh-mm-ss";
+import { Guild, VoiceChannel, VoiceConnection, StreamOptions, TextChannel, Message, DMChannel, GroupDMChannel } from "discord.js";
 import { LoggerInstance } from "winston";
 import { getDiscordGuildPlayerLogger } from "../../logging/logger";
 import { Readable } from "stream";
 import * as ytdl from "ytdl-core";
 
-enum PlayerStatus {
-    STOPPED, PAUSED, PLAYING
-}
+const MANUAL_STOP: string = "manual_stop";
+const SKIP: string = "skip";
 
 type StreamProvider = () => Readable;
 
 interface PlaylistItem {
-    stream: StreamProvider;
+    getStream: StreamProvider;
+    name: string;
 }
 
-class GuildPlayer extends EventEmitter implements Handler {
+type GuildPlayerOptions = {
+    discordPlaybackOptions?: StreamOptions,
+    ytdlOptions?: ytdl.downloadOptions
+};
+
+class GuildPlayer implements Handler {
 
     private playlist: PlaylistItem[] = [];
-    private playerStatus: PlayerStatus = PlayerStatus.STOPPED;
     private logger: LoggerInstance;
 
-    constructor(private guild: Guild) {
-        super();
+    constructor(private guild: Guild, private options: GuildPlayerOptions = {}) {
         this.logger = getDiscordGuildPlayerLogger(guild);
+
+        // Default to only stream audio
+        if (!options.ytdlOptions)
+            options.ytdlOptions = { filter: "audioonly" };
+        else if (!options.ytdlOptions.filter)
+            options.ytdlOptions.filter = "audioonly";
         let router = new CommandRouter();
 
-        router.use("yt", { handle: (cmd, resp) => {
-            if (cmd.args.length == 1) {
-                this.enqueue({ stream: () => {
-                    return ytdl(cmd.args[0], { filter: "audioonly" });
-                }});
-                if (!this.guild.voiceConnection && cmd.message.member && cmd.message.member.voiceChannel) {
-                    this.join(cmd.message.member.voiceChannel).then(this.start.bind(this));
-                }
-            }
-        }});
+        router.use("play", (cmd, resp) => {
+            if (cmd.args.length == 1 && (cmd.args[0].includes("youtube.com") || cmd.args[0].includes("youtu.be"))) {
+                ytdl.getInfo(cmd.args[0]).then(info => {
+                    this.enqueue({
+                        getStream: () => ytdl.downloadFromInfo(info, this.options.ytdlOptions),
+                        name: `${info.title} [${fromS(+info.length_seconds)}]`
+                    }, (cmd.message.member && cmd.message.member.voiceChannel) ? cmd.message.member && cmd.message.member.voiceChannel : undefined);
+                });
+            } else
+                resp.reply("Invalid `<song>` argument!");
+        });
+
+        router.use("stop", () => {
+            this.stop();
+            this.leave();
+        });
+        router.use("skip", this.skip.bind(this));
+        router.use("pause", this.pause.bind(this));
+        router.use("resume", this.start.bind(this));
+        router.use("pl", cmd => this.printPlaylist(cmd.message.channel));
 
         this.handle = router.handle.bind(router);
     }
 
     leave(): void {
-        if (this.guild.voiceConnection) {
-            this.stop();
+        if (this.guild.voiceConnection)
             this.guild.voiceConnection.disconnect();
-        }
     }
 
     join(channel: VoiceChannel): Promise<VoiceConnection> {
-        return channel.join();
+        if (channel.guild.id === this.guild.id && !this.connected)
+            return channel.join();
+        return Promise.reject("Channel not in this players guild or already connected");
+    }
+
+    printPlaylist(channel: TextChannel | DMChannel | GroupDMChannel): Promise<Message | Message[]> {
+        return channel.send(this.playlist.map(entry => entry.name).join("\n"), { split: true, code: true });
+    }
+
+    skip(): void {
+        if (this.guild.voiceConnection && this.guild.voiceConnection.dispatcher)
+            this.guild.voiceConnection.dispatcher.end(SKIP);
     }
 
     stop(): void {
         if (this.guild.voiceConnection && this.guild.voiceConnection.dispatcher) {
-            this.playerStatus = PlayerStatus.STOPPED;
-            this.guild.voiceConnection.dispatcher.end();
-            this.emit("stopped");
+            this.playlist = [];
+            this.guild.voiceConnection.dispatcher.end(MANUAL_STOP);
         }
     }
 
     pause(): void {
-        if (this.guild.voiceConnection && this.guild.voiceConnection.dispatcher) {
-            this.playerStatus = PlayerStatus.PAUSED;
+        if (this.guild.voiceConnection && this.guild.voiceConnection.dispatcher)
             this.guild.voiceConnection.dispatcher.pause();
-            this.emit("paused");
-        }
     }
 
+    /**
+     * Start playing in the connected channel
+     */
     start(): void {
-        // Start playing in connected channel
-        if (this.guild.voiceConnection) {
+        if (this.connected) {
             // If there is a paused playback resume it
-            if (this.guild.voiceConnection.dispatcher) {
-                if (this.guild.voiceConnection.dispatcher.paused) {
-                    this.playerStatus = PlayerStatus.PLAYING;
-                    this.guild.voiceConnection.dispatcher.resume();
-                    this.emit("started");
-                }
-                console.log("tax fraud");
+            if (this.guild.voiceConnection.dispatcher && this.guild.voiceConnection.dispatcher.paused) {
+                this.guild.voiceConnection.dispatcher.resume();
             } else {
                 let item = this.playlist.shift();
                 if (item) {
-                    this.playerStatus = PlayerStatus.PLAYING;
-                    this.guild.voiceConnection.playStream(item.stream()).on("end", () => {
+                    this.guild.voiceConnection.playStream(item.getStream(), this.options.discordPlaybackOptions).on("end", reason => {
                         // If player hasn't been stopped manually continue playing
-                        if (this.playerStatus == PlayerStatus.PLAYING)
+                        if (reason !== MANUAL_STOP)
                             this.start();
                     });
                 } else {
+                    // If no more songs in the queue leave
                     this.leave();
                 }
             }
         }
     }
 
-    enqueue(item: PlaylistItem) {
+    /**
+     * Add an item to the playlist
+     * @param item The item to be added
+     * @param autoJoin If not null or undefined join the provided channel and start playback
+     */
+    enqueue(item: PlaylistItem, autoJoinChannel?: VoiceChannel): void {
         this.playlist.push(item);
-        this.emit("songEnqued", item);
+        if (autoJoinChannel && !this.connected)
+            this.join(autoJoinChannel).then(this.start.bind(this));
+    }
+
+    get connected(): boolean {
+        return Boolean(this.guild.voiceConnection);
     }
 
     handle: HandleCallback;
 }
 
+export type DiscordPlayerOptions = GuildPlayerOptions;
+
 export class DiscordPlayer implements Handler {
 
-    constructor() {
-        let guildContext = new GuildCommandContext(guild => new GuildPlayer(guild));
+    constructor(private options?: DiscordPlayerOptions) {
+        let guildContext = new GuildCommandContext(guild => new GuildPlayer(guild, options));
         this.handle = guildContext.handle.bind(guildContext);
     }
 
